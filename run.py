92cbab257e47649d1379454d989ca7895a3b837c
oonyekpe <obiageli_o@hotmail.com>
2022-05-28 15:09:33 +0000

    added break

--- run.py
+++ run.py
@@ -265,6 +265,7 @@ def collect_availability():
             user_input = input("Enter your answer here:\n").upper().strip()
             if user_input not in [1, 2, "1", "2"]:
                 print("invalid entry, please try again")
+                break
         user_availability.append(user_input)
 
     return user_availability
